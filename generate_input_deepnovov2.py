d325255ea76c3743eac0fcee5bafc61e1f6b860b
Samuel Diebolt <stryars@gmail.com>
2020-05-20 14:53:59 +0200

    Fixed issue in the input files generation script
    The MGF formatting was done at the end. This step includes deleting all
    empty spectra. This meant that the merged features file contained
    references to scans that didn't exist anymore. This is now fixed by
    first formatting each individual MGF file before extracting the
    features.

--- generate_input_deepnovov2.py
+++ generate_input_deepnovov2.py
@@ -9,20 +9,30 @@ import smbp_deepnovov2_tools as sdt
 # Extract features.
 mgf_files = glob.glob('smbp_data/*.dat.mgf')
 for mgf_file in mgf_files:
+    print('Extracting features from: {}'.format(mgf_file))
+
+    mgf_file_formatted = '{0}_formatted{1}'.format(*os.path.splitext(mgf_file))
+    mascot_file = os.path.splitext(mgf_file)[0]+'.xml'
+    features_file = (
+        "{0}_features.csv"
+        .format(os.path.splitext(mgf_file_formatted)[0])
+    )
+
+    # Reformat MGF file to comply with DeepNovoV2.
+    sdt.format_mgf_deepnovo(mgf_file, mgf_file_formatted)
+    print('Formatted MGF: {}'.format(mgf_file_formatted))
+
     # Extract features for current MGF file.
     features = sdt.extract_features(
-        mgf_file, os.path.splitext(mgf_file)[0]+'.xml'
+        mgf_file_formatted, mascot_file
     )
+
     # Save features to CSV, appending _features to the filename.
-    features.to_csv(
-        "{0}_{1}.csv".format(os.path.splitext(mgf_file)[0], 'features'),
-        index=False
-    )
+    features.to_csv(features_file, index=False)
+    print('Extracted features: {}\n'.format(features_file))
 
 # Merge MGF and features files.
+mgf_formatted_files = glob.glob('smbp_data/*_formatted.mgf')
 features_files = glob.glob('smbp_data/*_features.csv')
-sdt.merge_mgf(mgf_files, 'spectrum_smbp.mgf')
+sdt.merge_mgf(mgf_formatted_files, 'spectrum_smbp.mgf')
 sdt.merge_features(features_files, 'features_smbp.csv')
-
-# Reformat MGF file to comply with DeepNovoV2.
-sdt.format_mgf_deepnovo('spectrum_smbp.mgf', 'spectrum_smbp_formatted.mgf')
